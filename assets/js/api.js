75e18243ae958afc8aaa2eea7aa8d3b0fa81fe23
Kauã Nogueira <81389414+kauaumnougueira@users.noreply.github.com>
2023-12-01 10:27:22 -0300

    loucura insanidade

--- assets/js/api.js
+++ assets/js/api.js
@@ -17,3 +17,24 @@ function get(token, rota){
 }
 
 export { get }
+
+function post(rota, dado){
+    return fetch('http://127.0.0.1:8000/api/criar-' + rota, {
+        method: 'POST',  // Corrigido para 'POST'
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRF-TOKEN': token
+        },
+        mode: 'cors',
+        body: JSON.stringify(dado)  // Incluído o corpo da requisição com os dados
+    })
+    .then(response => response.json())
+    .then(data => {
+        return data;
+    })
+    .catch(error => { 
+        console.error(error);
+    });
+}
+
+export { post }
---
+++ assets/js/endereco/endereco.js
@@ -0,0 +1,7 @@
+checkboxFormat("#checkboxes1")
+mascaras()
+const cep1 = document.getElementById('inputCep1')
+cep1.addEventListener('keyup', () => {
+    if (cep1.value.length === 9 && !cep1.value.includes('_'))
+        apiCep(cep1.value, '1')
+})
